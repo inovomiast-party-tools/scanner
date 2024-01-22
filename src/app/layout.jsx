f38fe741f5b45061e5677b014b7949b156f7bd26
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2024-01-22 17:33:32 +0100

    Update layout.jsx Title to: INPTScanner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "INTPScanner",
+  title: "INPTScanner",
   description: "Ticket QrCode Scanner for INovomiast Parties",
 };
 
